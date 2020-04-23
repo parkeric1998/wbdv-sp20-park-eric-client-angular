741f002abce1df690c14ded3978526d056312b90
Eric Park <43224005+parkeric1998@users.noreply.github.com>
2020-04-22 20:05:57 -0400

    Fixed CORS policy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import {CourseListComponent} from './course-list/course-list.component';
 import {CourseViewerComponent} from './course-viewer/course-viewer.component';
 import {HomeComponent} from './home/home.component';
-
+import {QuizzesComponent} from './quizzes/quizzes.component';
+import {QuizComponent} from './quiz/quiz.component';
 
 const routes: Routes = [
+  {path: 'quizzes/:quizId', component: QuizComponent},
+  {path: 'quizzes', component: QuizzesComponent},
   {path: '', component: HomeComponent},
   {path: 'courses', component: CourseListComponent},
-  {path: 'course/:cid', component: CourseViewerComponent},
-  {path: 'course/:cid/module/:mid', component: CourseViewerComponent},
-  {path: 'course/:cid/module/:mid/lesson/:lid', component: CourseViewerComponent}
+  {path: 'details/:courseId', component: CourseViewerComponent},
+  {path: 'details/:courseId/modules/:moduleId', component: CourseViewerComponent},
+  {path: 'details/:courseId/modules/:moduleId/lessons/:lessonId', component: CourseViewerComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { CourseViewerComponent } from './course-viewer/course-viewer.component';
 import { HomeComponent } from './home/home.component';
 import { ModuleListComponent } from './module-list/module-list.component';
 import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
+import { QuizzesComponent } from './quizzes/quizzes.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { TrueFalseQuestionComponent } from './true-false-question/true-false-question.component';
+import { MultipleChoiceQuestionComponent } from './multiple-choice-question/multiple-choice-question.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
     CourseViewerComponent,
     HomeComponent,
     ModuleListComponent,
-    LessonTabsComponent
+    LessonTabsComponent,
+    QuizzesComponent,
+    QuizComponent,
+    TrueFalseQuestionComponent,
+    MultipleChoiceQuestionComponent
   ],
     imports: [
         BrowserModule,
